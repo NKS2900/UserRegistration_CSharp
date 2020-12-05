ecc30d1f794a79c07197567c0c872fc730f1390d
unknown <nksayyad2900.com>
2020-12-05 09:57:11 +0530

    [NIJAM] Add:LastName Validation.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace UserRegistrationValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
-            FirstNameVal();         
+            FirstNameVal();
+            LastNameVal();
         }
         public static void FirstNameVal()
         {           
@@ -21,6 +22,17 @@ namespace UserRegistrationValidation
                 Console.WriteLine("Name is Valid : " + first_name);
             else
                 Console.WriteLine("Invalid FirstName");           
+        }
+        public static void LastNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter Last Name");
+            string last_name = Console.ReadLine();
+            bool validate = rg.IsMatch(last_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + last_name);
+            else
+                Console.WriteLine("Invalid LastName");
         }
     }
 }
